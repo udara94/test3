1597d6006827b9ae0e474c035daaeccf0b4e25aa
udara94 <37542994+udara94@users.noreply.github.com>
2018-06-12 09:37:05 +0530

    first commit

---
+++ src/test3/Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test3.Model
+{
+    public class Customer
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement]
+        public int CustomerId { get; set; }
+        [BsonElement]
+        public string CustomerName { get; set; }
+        [BsonElement]
+        public string Address { get; set; }
+    }
+}
